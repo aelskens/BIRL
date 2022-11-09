d7164c46bd9c5ced0f1060f7d89409790d22d5cb
aelskens <arthur.elskens@ulb.be>
2022-11-09 17:13:44 +0100

    all configs in shell script

--- experiments/run-pipeline.sh
+++ experiments/run-pipeline.sh
@@ -13,15 +13,21 @@ read percentage
 echo Config?
 read config
 
+
+if [[ $config == "all" ]];
+then
+    configs=("xavier" "birl" "LowRes")
+else
+    configs=($config)
+fi
+
 table="/io/inputs/dataset_ANHIR/${tissue}_dataset_medium_${percentage}pc.csv"
 # this folder has to contain bland of images and landmarks
 dataset="/io/inputs/dataset_ANHIR/images"
 results="/io/outputs/dataset_ANHIR/"
 apps="/BIRL/Applications"
 
-preprocessings=("gray")
-
-for pproc in "${preprocessings[@]}"
+for c in "${configs[@]}"
 do
 
     python experiments/low-high_res_elastix.py \
@@ -29,7 +35,7 @@ do
          -d $dataset \
          -o $results \
          -elastix "$apps/elastix/bin" \
-         -cfg "./configs/elastix_${config}_affine.txt" \
+         -cfg "./configs/elastix_${c}_affine.txt" \
          --visual --unique --nb_workers $jobs
 
 done
