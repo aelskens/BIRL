ce06cc5ebf4968ff76ed4e42e4ce6fec2812d961
aelskens <arthur.elskens@ulb.be>
2022-11-29 19:48:51 +0100

    run_pipeline upgrade to automate PS evaluation

--- experiments/run-pipeline.sh
+++ experiments/run-pipeline.sh
@@ -2,17 +2,25 @@
 
 export DISPLAY=""
 # DEFINE GLOBAL PARAMS
-jobs=3
+jobs=2
 
 echo Tissue?
 read tissue
 
-echo Percentage
-read percentage
+if [[ $tissue == "all" ]];
+then
+    low_tissue=("coad" "gastric" "kidney" "lung-lesion" "lung-lobes" "mammary-gland" "mice-kidney")
+    low_tissue2=("10" "2.5" "2.5" "2.5" "10" "10" "5")
+else
+    low_tissue=("$tissue")
+
+    echo Percentage?
+    read percentage
+    low_tissue2=("$percentage")
+fi 
 
 echo Config?
 read config
-
 
 if [[ $config == "all" ]];
 then
@@ -21,21 +29,35 @@ else
     configs=($config)
 fi
 
-table="/io/inputs/dataset_ANHIR/${tissue}_dataset_medium_${percentage}pc.csv"
 # this folder has to contain bland of images and landmarks
 dataset="/io/inputs/dataset_ANHIR/images"
-results="/io/outputs/dataset_ANHIR/"
+parameter_sets=/io/inputs/dataset_ANHIR/segmentation_ps/*.json
 apps="/BIRL/Applications"
 
-for c in "${configs[@]}"
+for i in "${!low_tissue[@]}"
 do
+    ti=${low_tissue[i]}
+    perc=${low_tissue2[i]}
+    table="/io/inputs/dataset_ANHIR/${ti}_dataset_medium_${perc}pc.csv"   
+    results="/io/outputs/dataset_ANHIR/PS_evaluation/${ti}" 
+    
+    for ps in $parameter_sets
+    do
+        ps_name=$(basename -- "$ps" .json)
+        mkdir ${results}/${ps_name}
 
-    python experiments/low-high_res_elastix.py \
-         -t $table \
-         -d $dataset \
-         -o $results \
-         -elastix "$apps/elastix/bin" \
-         -cfg "./configs/elastix_${c}_affine.txt" \
-         --visual --unique --nb_workers $jobs
+        for c in "${configs[@]}"
+        do
 
+            python experiments/low-high_res_elastix.py \
+                -t $table \
+                -d $dataset \
+                -o ${results}/${ps_name} \
+                -elastix "$apps/elastix/bin" \
+                -cfg "./configs/elastix_${c}_affine.txt" \
+                -sgm_params $ps \
+                --visual --unique --nb_workers $jobs
+
+        done
+    done
 done
