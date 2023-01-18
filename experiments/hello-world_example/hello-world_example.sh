433211d119537f83d027af7834849eeec2ff2757
aelskens <arthurelsk@gmail.com>
2023-01-18 16:15:00 +0100

    small fix

--- experiments/hello-world_example/hello-world_example.sh
+++ experiments/hello-world_example/hello-world_example.sh
@@ -13,5 +13,5 @@ python experiments/low-high_res_elastix.py \
     -o "/outputs" \
     -elastix "$apps/elastix/bin" \
     -cfg "./configs/elastix_LowRes_affine.txt" \
-    -sgm_params "/io/inputs/dataset_ANHIR/segmentation_ps/ps_06.json" \
+    -sgm_params "./experiment/prealignment_ps/best_ps.json" \
     --visual --unique --nb_workers $jobs
